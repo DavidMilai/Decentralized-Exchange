fe7a25ccabf748c518dee20ca46bec46166e0882
David Milai <davidmilai67@gmail.com>
2022-12-20 14:29:41 +0300

    comment

--- hardhat-tutorial/contracts/Exchange.sol
+++ hardhat-tutorial/contracts/Exchange.sol
@@ -119,13 +119,7 @@ function cryptoDevTokenToEth(uint _tokensSold, uint _minEth) public {
 }
 
 function ethToCryptoDevToken(uint _minTokens) public payable {
-    uint256 tokenReserve = getReserve();
-    // call the `getAmountOfTokens` to get the amount of Crypto Dev tokens
-    // that would be returned to the user after the swap
-    // Notice that the `inputReserve` we are sending is equal to
-    // `address(this).balance - msg.value` instead of just `address(this).balance`
-    // because `address(this).balance` already contains the `msg.value` user has sent in the given call
-    // so we need to subtract it to get the actual input reserve
+    uint256 tokenReserve = getReserve(); 
     uint256 tokensBought = getAmountOfTokens(
         msg.value,
         address(this).balance - msg.value,
