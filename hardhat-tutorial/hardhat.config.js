435b65e69beb2ecaf7c1b77769f41d5e04c28442
David Milai <davidmilai67@gmail.com>
2023-01-03 16:49:22 +0300

    compiled contract

--- hardhat-tutorial/hardhat.config.js
+++ hardhat-tutorial/hardhat.config.js
@@ -1,5 +1,7 @@
-require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
+require("@nomiclabs/hardhat-waffle");
+
+
 
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
