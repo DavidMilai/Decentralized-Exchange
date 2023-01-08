23b1eb4d6ed9a85ccf78bf6ae8905c946f0660ef
David Milai <davidmilai67@gmail.com>
2023-01-08 19:54:24 +0300

    imports

--- frontend/utils/addLiquidity.js
+++ frontend/utils/addLiquidity.js
@@ -1,10 +1,11 @@
-import { Contract, utils } from "ethers";
 import {
   EXCHANGE_CONTRACT_ABI,
   EXCHANGE_CONTRACT_ADDRESS,
   TOKEN_CONTRACT_ABI,
   TOKEN_CONTRACT_ADDRESS,
 } from "../constants";
+import { Contract, utils } from "ethers";
+
  
 export const addLiquidity = async (
   signer,
